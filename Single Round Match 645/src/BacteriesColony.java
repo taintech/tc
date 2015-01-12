5faf3be3f0885cbe7f495df3f7ef5b18e5eaed9e
taintech <rinattainov@gmail.com>
2015-01-13 01:04:45 +0200

    SRM 645 just formatted more nicely

--- Single Round Match 645/src/BacteriesColony.java
+++ Single Round Match 645/src/BacteriesColony.java
@@ -1,24 +1,25 @@
 import java.util.*;
 import java.math.*;
+
 import static java.lang.Math.*;
 
 public class BacteriesColony {
-	
-	public int[] performTheExperiment(int[] colonies) {
-		int n = colonies.length;
-		int[] result = Arrays.copyOf(colonies, n);
-		do{
-			colonies = Arrays.copyOf(result, n);
-			for (int i = 1; i < n-1; i++) {
-				int current = colonies[i];
-				int left = colonies[i-1];
-				int right = colonies[i+1];
-				if(current>left&&current>right)
-					result[i] = current - 1;
-				else if(current<left&&current<right)
-					result[i] = current + 1;
-			}
-		}while (!Arrays.equals(result, colonies));
-		return result;
-	}
+
+    public int[] performTheExperiment(int[] colonies) {
+        int n = colonies.length;
+        int[] result = Arrays.copyOf(colonies, n);
+        do {
+            colonies = Arrays.copyOf(result, n);
+            for (int i = 1; i < n - 1; i++) {
+                int current = colonies[i];
+                int left = colonies[i - 1];
+                int right = colonies[i + 1];
+                if (current > left && current > right)
+                    result[i] = current - 1;
+                else if (current < left && current < right)
+                    result[i] = current + 1;
+            }
+        } while (!Arrays.equals(result, colonies));
+        return result;
+    }
 }
