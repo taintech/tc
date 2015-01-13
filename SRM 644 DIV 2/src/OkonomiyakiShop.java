7fdec3d105a6ba9a8f46ef1780fb14c296a86a05
taintech <rinattainov@gmail.com>
2015-01-13 10:22:39 +0200

    SRM 644 first question

--- SRM 644 DIV 2/src/OkonomiyakiShop.java
+++ SRM 644 DIV 2/src/OkonomiyakiShop.java
@@ -5,6 +5,17 @@ import static java.lang.Math.*;
 public class OkonomiyakiShop {
 	
 	public int count(int[] osize, int K) {
-		return 0;
+		Arrays.sort(osize);
+		int sum = 0;
+		for (int i = 0; i < osize.length; i++) {
+			int cur = osize[i];
+			int count = 0;
+			for (int j = i + 1; j < osize.length; j++) {
+				int next = osize[j];
+				if(next-cur <= K) count++;
+			}
+			sum += count;
+		}
+		return sum;
 	}
 }
